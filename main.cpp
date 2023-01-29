bbeb747b6a6de32e7905e3c1b8f8b2866a525f30
NoahAlban <78056445+NoahAlban@users.noreply.github.com>
2023-01-29 17:14:24 -0600

    Put the files on github

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//code for Problem Set about memory management
+#include<iostream>
+using namespace std;
+int main(){
+    std::cout << "Hello World" << std::endl;
+    cout << "ha" << endl;
+    int p;
+    int x[3];
+    return 0;
+}
